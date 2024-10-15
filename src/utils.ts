4c9b97e3f3c146d091e54bc29d228a3d257829ab
fschlent <frank.schlenter@arkestro.com>
2024-10-15 16:11:03 -0400

    bold labels word

--- src/utils.ts
+++ src/utils.ts
@@ -76,7 +76,7 @@ export const buildPRDescription = (details: JIRADetails) => {
   const listAsString = details.labels.join(' ');
   return `<table><tbody><tr><td><a href="${details.url}" title="${displayKey}" target="_blank"><img alt="${details.type.name}" src="${details.type.icon}" /> ${displayKey}</a>
   ${details.summary}</td><td>${details.type.name}</td></tr></tbody></table><br />
-  ${listAsString}
+  **Labels:** ${listAsString}
 
   ${details.description}`;
 };
